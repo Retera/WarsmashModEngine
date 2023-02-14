78edb4d9e72c8cdea406835cc3aaade91ed448c3
bear_369 <98942034+bearU369@users.noreply.github.com>
2023-02-14 21:48:12 +0800

    Map items display their respective map icon UI and maxPlayers counter.
    - Each map item can now display their own mapIcon and maxPlayers counter..

--- core/src/com/etheller/warsmash/parsers/fdf/frames/ListBoxFrame.java
+++ core/src/com/etheller/warsmash/parsers/fdf/frames/ListBoxFrame.java
@@ -15,7 +15,9 @@ import com.etheller.warsmash.parsers.fdf.GameUI;
 import com.etheller.warsmash.parsers.fdf.datamodel.FramePoint;
 import com.etheller.warsmash.parsers.fdf.datamodel.TextJustify;
 import com.etheller.warsmash.parsers.w3x.War3Map;
+import com.etheller.warsmash.parsers.w3x.w3i.War3MapW3i;
 import com.etheller.warsmash.parsers.w3x.objectdata.Warcraft3MapObjectData;
+import com.etheller.warsmash.parsers.w3x.w3i.War3MapW3iFlags;
 import com.etheller.warsmash.units.custom.WTS;
 import com.etheller.warsmash.viewer5.handlers.w3x.War3MapViewer;
 public class ListBoxFrame extends ControlFrame implements ScrollBarFrame.ScrollBarChangeListener {
@@ -24,7 +26,11 @@ public class ListBoxFrame extends ControlFrame implements ScrollBarFrame.ScrollB
 	private static final Color MOUSE_OVER_HIGHLIGHT_COLOR = new Color(0.3f, 0.3f, 1.0f, 0.25f);
 
 	private final List<String> listItems = new ArrayList<>();
+
 	private final List<SingleStringFrame> stringFrames = new ArrayList<>();
+	private final List<SingleStringFrame> maxPlayersValueFrames = new ArrayList<>();
+	private final List<BackdropFrame> maxPlayersIconFrames = new ArrayList<>();
+	
 	private BitmapFont frameFont;
 	private float listBoxBorder;
 	private int selectedIndex = -1;
@@ -88,6 +94,14 @@ public class ListBoxFrame extends ControlFrame implements ScrollBarFrame.ScrollB
 	}
 
 	private void positionChildren(final GameUI gameUI, final Viewport viewport) {
+		for (final BackdropFrame frame: this.maxPlayersIconFrames) {
+			if (frame == null) continue;
+			frame.positionBounds(gameUI, viewport);
+		}
+		for (final SingleStringFrame frame : this.maxPlayersValueFrames) {
+			if (frame == null) continue;
+			frame.positionBounds(gameUI, viewport);
+		}
 		for (final SingleStringFrame frame : this.stringFrames) {
 			frame.positionBounds(gameUI, viewport);
 		}
@@ -103,8 +117,23 @@ public class ListBoxFrame extends ControlFrame implements ScrollBarFrame.ScrollB
 		super.internalRender(batch, baseFont, glyphLayout);
 		this.selectionFrame.render(batch, baseFont, glyphLayout);
 		this.mouseHighlightFrame.render(batch, baseFont, glyphLayout);
-		for (final SingleStringFrame frame : this.stringFrames) {
-			frame.render(batch, baseFont, glyphLayout);
+		// for (final SingleStringFrame frame : this.stringFrames) {
+		// 	frame.render(batch, baseFont, glyphLayout);
+		// }
+		// for (final BackdropFrame frame : this.maxPlayersIconFrames) {
+		// 	if(frame == null) continue;
+		// 	frame.render(batch, baseFont, glyphLayout);
+		// }
+		// for (final SingleStringFrame frame : this.maxPlayersValueFrames) {
+		// 	if(frame == null) continue;
+		// 	frame.render(batch, baseFont, glyphLayout);
+		// }
+		for (int i = 0; i < this.stringFrames.size(); i++) {
+			this.stringFrames.get(i).render(batch, baseFont, glyphLayout);
+			if (this.maxPlayersIconFrames.get(i) != null)
+				this.maxPlayersIconFrames.get(i).render(batch, baseFont, glyphLayout);
+			if (this.maxPlayersValueFrames.get(i) != null)
+				this.maxPlayersValueFrames.get(i).render(batch, baseFont, glyphLayout);
 		}
 		if (this.scrollBarFrame != null) {
 			this.scrollBarFrame.render(batch, baseFont, glyphLayout);
@@ -153,51 +182,110 @@ public class ListBoxFrame extends ControlFrame implements ScrollBarFrame.ScrollB
 	}
 
 	private void updateUI(final GameUI gameUI, final Viewport viewport) {
-		SingleStringFrame prev = null;
+		AbstractRenderableFrame prev = null;
 		boolean foundSelected = false;
 		boolean foundMouseOver = false;
+		// final int numStringFrames = (int) Math.min(this.listItems.size(),
+		// 		(Math.floor((this.renderBounds.height - (this.listBoxBorder * 2)) / (this.frameFont.getLineHeight()))));
 		final int numStringFrames = (int) Math.min(this.listItems.size(),
-				(Math.floor((this.renderBounds.height - (this.listBoxBorder * 2)) / (this.frameFont.getLineHeight()))));
+				(Math.floor((this.renderBounds.height - (this.listBoxBorder * 2)) / 30)));
+
+		final StringFrame maxPlayerOriginal = (StringFrame) gameUI.getFrameByName("MaxPlayersValue", 0);
 
 		final int scrollOffset = computeScrollOffset(numStringFrames);
 		if (numStringFrames != this.stringFrames.size()) {
 			this.stringFrames.clear();
+			this.maxPlayersIconFrames.clear();
+			this.maxPlayersValueFrames.clear();
 			for (int stringFrameIndex = 0; stringFrameIndex < numStringFrames; stringFrameIndex++) {
 				final int index = stringFrameIndex + scrollOffset;
 				final boolean selected = (index == this.selectedIndex);
 				final boolean mousedOver = (index == this.mouseOverIndex);
 				final SingleStringFrame stringFrame = new SingleStringFrame("LISTY" + index, this, Color.WHITE,
 						TextJustify.LEFT, TextJustify.MIDDLE, this.frameFont);
+				SingleStringFrame maxPlayersValueFrame = new SingleStringFrame("MapValueY_" + index, this, Color.YELLOW, TextJustify.CENTER, TextJustify.MIDDLE, maxPlayerOriginal.getFrameFont());
+				BackdropFrame maxPlayersIconFrame =  (BackdropFrame) gameUI.createFrameByType("BACKDROP", "MapIconY_" + index, this, "", 0);;
 				if (index < this.listItems.size()) {
 					// stringFrame.setText(this.listItems.get(index));
 					try {
 						War3Map map = War3MapViewer.beginLoadingMap(gameUI.getDataSource(), this.listItems.get(index));
+						War3MapW3i mapInfo = map.readMapInformation();
 						WTS objDat = Warcraft3MapObjectData.loadWTS(map);
 						gameUI.setMapStrings(objDat);
-						stringFrame.setText(gameUI.getTrigStr(map.readMapInformation().getName()));
+						stringFrame.setText(gameUI.getTrigStr(mapInfo.getName()));
+
+						// maxPlayersValueFrame = (StringFrame) gameUI.getFrameByName("MaxPlayersValue", 0);
+						maxPlayersValueFrame.setText(Integer.toString(mapInfo.getPlayers().size()));
+
+						// maxPlayersIconFrame = (BackdropFrame) gameUI.getFrameByName("MaxPlayersIcon", 0);
+						maxPlayersIconFrame.setWidth(30);
+						maxPlayersIconFrame.setHeight(30);
+						if (mapInfo.hasFlag(War3MapW3iFlags.MELEE_MAP)){
+							maxPlayersIconFrame.setBackground(gameUI.loadTexture("ui\\widgets\\glues\\icon-file-melee.blp"));
+						} else {
+							maxPlayersIconFrame.setBackground(gameUI.loadTexture("ui\\widgets\\glues\\icon-file-ums.blp"));
+						}
 					}catch(Exception e){
 						e.printStackTrace();
 					}
 				}
-				stringFrame.setWidth(this.renderBounds.width - (this.listBoxBorder * 2));
+				// stringFrame.setWidth(this.renderBounds.width - (this.listBoxBorder * 2));
+				stringFrame.setWidth(this.renderBounds.width - (this.listBoxBorder * 2) - 30);
 				stringFrame.setHeight(this.frameFont.getLineHeight());
 				if (prev != null) {
-					stringFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, prev, FramePoint.BOTTOMLEFT, 0, 0));
+					// stringFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, prev, FramePoint.BOTTOMLEFT, 0, 0));
+					if (maxPlayersIconFrame == null) {
+						stringFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, prev, FramePoint.BOTTOMLEFT,0, 0));
+						prev = stringFrame;
+					} else {
+						maxPlayersIconFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, prev, FramePoint.BOTTOMLEFT,
+							0, 0));
+						maxPlayersValueFrame.addSetPoint(new SetPoint(FramePoint.CENTER, maxPlayersIconFrame, FramePoint.CENTER,
+							0, 0));
+						stringFrame.addSetPoint(new SetPoint(FramePoint.LEFT, maxPlayersIconFrame, FramePoint.RIGHT,
+							0, 0));
+						prev = maxPlayersIconFrame;
+					}
 				} else {
-					stringFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, this, FramePoint.TOPLEFT,
+					// stringFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, this, FramePoint.TOPLEFT,
+					// 	this.listBoxBorder, -this.listBoxBorder));
+					// prev = stringFrame;
+					if (maxPlayersIconFrame == null) {
+						stringFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, this, FramePoint.TOPLEFT,
 							this.listBoxBorder, -this.listBoxBorder));
+						prev = stringFrame;
+					} else {
+						maxPlayersIconFrame.addSetPoint(new SetPoint(FramePoint.TOPLEFT, this, FramePoint.TOPLEFT,
+							this.listBoxBorder, -this.listBoxBorder));
+						maxPlayersValueFrame.addSetPoint(new SetPoint(FramePoint.CENTER, maxPlayersIconFrame, FramePoint.CENTER,
+							0, 0));
+						stringFrame.addSetPoint(new SetPoint(FramePoint.LEFT, maxPlayersIconFrame, FramePoint.RIGHT,
+							0, 0));
+						prev = maxPlayersIconFrame;
+					}
+
 				}
 				this.stringFrames.add(stringFrame);
-				prev = stringFrame;
+				this.maxPlayersIconFrames.add(maxPlayersIconFrame);
+				this.maxPlayersValueFrames.add(maxPlayersValueFrame);
+				// prev = stringFrame;
 				if (selected) {
+					// this.selectionFrame
+					// 		.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+					// this.selectionFrame.addSetPoint(
+					// 		new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					this.selectionFrame
-							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, maxPlayersIconFrame, FramePoint.TOPLEFT, 0, 0));
 					this.selectionFrame.addSetPoint(
 							new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					foundSelected = true;
 				} else if (mousedOver) {
+					// this.mouseHighlightFrame
+					// 		.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+					// this.mouseHighlightFrame.addSetPoint(
+					// 		new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					this.mouseHighlightFrame
-							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, maxPlayersIconFrame, FramePoint.TOPLEFT, 0, 0));
 					this.mouseHighlightFrame.addSetPoint(
 							new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					foundMouseOver = true;
@@ -209,26 +297,45 @@ public class ListBoxFrame extends ControlFrame implements ScrollBarFrame.ScrollB
 				final boolean selected = (index == this.selectedIndex);
 				final boolean mousedOver = (index == this.mouseOverIndex);
 				final SingleStringFrame stringFrame = this.stringFrames.get(stringFrameIndex);
+				SingleStringFrame maxPlayersValueFrame = this.maxPlayersValueFrames.get(stringFrameIndex);
+				BackdropFrame maxPlayersIconFrame = this.maxPlayersIconFrames.get(stringFrameIndex);
 				if (index < this.listItems.size()) {
 					// stringFrame.setText(this.listItems.get(index));
 					try {
 						War3Map map = War3MapViewer.beginLoadingMap(gameUI.getDataSource(), this.listItems.get(index));
+						War3MapW3i mapInfo = map.readMapInformation();
 						WTS objDat = Warcraft3MapObjectData.loadWTS(map);
+
 						gameUI.setMapStrings(objDat);
-						stringFrame.setText(gameUI.getTrigStr(map.readMapInformation().getName()));
+						stringFrame.setText(gameUI.getTrigStr(mapInfo.getName()));
+						maxPlayersValueFrame.setText(Integer.toString(mapInfo.getPlayers().size()));
+
+						if (mapInfo.hasFlag(War3MapW3iFlags.MELEE_MAP)){
+							maxPlayersIconFrame.setBackground(gameUI.loadTexture("ui\\widgets\\glues\\icon-file-melee.blp"));
+						} else {
+							maxPlayersIconFrame.setBackground(gameUI.loadTexture("ui\\widgets\\glues\\icon-file-ums.blp"));
+						}
 					}catch(Exception e){
 						e.printStackTrace();
 					}
 				}
 				if (selected) {
+					// this.selectionFrame
+					// 		.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+					// this.selectionFrame.addSetPoint(
+					// 		new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					this.selectionFrame
-							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, maxPlayersIconFrame, FramePoint.TOPLEFT, 0, 0));
 					this.selectionFrame.addSetPoint(
 							new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					foundSelected = true;
 				} else if (mousedOver) {
+					// this.mouseHighlightFrame
+					// 		.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+					// this.mouseHighlightFrame.addSetPoint(
+					// 		new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					this.mouseHighlightFrame
-							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, stringFrame, FramePoint.TOPLEFT, 0, 0));
+							.addSetPoint(new SetPoint(FramePoint.TOPLEFT, maxPlayersIconFrame, FramePoint.TOPLEFT, 0, 0));
 					this.mouseHighlightFrame.addSetPoint(
 							new SetPoint(FramePoint.BOTTOMRIGHT, stringFrame, FramePoint.BOTTOMRIGHT, 0, 0));
 					foundMouseOver = true;
